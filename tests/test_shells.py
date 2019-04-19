4b3d9149742711771eca44f7392afd4afd98d840
Gram <master_fess@mail.ru>
2019-04-19 16:12:13 +0300

    fix flake

--- tests/test_shells.py
+++ tests/test_shells.py
@@ -4,13 +4,13 @@ from dephell_shells import Shells
 
 def test_command():
     shells = Shells(bin_path=None)
-    for name, cls in shells.shells.items():
+    for cls in shells.shells.values():
         shell = cls(bin_path=Path(), shell_path=None)
         assert isinstance(shell.command, (list, str))
 
 
 def test_args():
     shells = Shells(bin_path=None)
-    for name, cls in shells.shells.items():
+    for cls in shells.shells.values():
         shell = cls(bin_path=Path(), shell_path=None)
         assert isinstance(shell.args, list)
